71cc4e07deab7a562643208bd6050ab516d741a4
jonmellman <jonmellman@gmail.com>
2018-11-20 18:03:56 +0100

    Clean up styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,16 @@ import Typography from 'typography';
 import lincoln from 'typography-theme-lincoln';
 
 lincoln.overrideThemeStyles = () => ({
+    html: {
+        backgroundColor: '#a8b5b2',
+    },
+    body: {
+        margin: '3rem auto',
+        maxWidth: '632px',
+        padding: '32px',
+        backgroundColor: 'white',
+        boxShadow: '20px 20px 20px 0px black',
+    },
     a: {
         textShadow: 'none',
         backgroundImage: 'none',
